8a8cd6e7a4feaf941eeb01eb004dcaaf7a857ff2
Stan <phobiozzz@gmail.com>
2020-04-07 18:53:49 +0300

    Create HPBar.cs

---
+++ NecromancerBook/Assets/Scripts/UiScripts/Bars/HPBar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class HPBar : MonoBehaviour
+{
+    public GameObject globeReference;
+    public List<GameObject> globes;
+
+    
+    public float globePosX = 30;
+    public float globePosY = -30;
+    public void Awake()
+    {
+        
+    }
+
+    public void AddGlobe()
+    {
+        GameObject globe = Instantiate(globeReference, new Vector3(globePosX, globePosY, 0), globeReference.transform.rotation);
+        globe.GetComponent<Transform>().SetParent(this.transform);
+        globe.GetComponent<RectTransform>().anchoredPosition = new Vector2(globePosX, globePosY);
+        globe.GetComponent<RectTransform>().localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        Debug.Log("Globe position " + globe.GetComponent<RectTransform>().anchoredPosition);
+        globePosX = globe.GetComponent<RectTransform>().anchoredPosition.x + 50;
+        globes.Add(globe);
+    }
+    
+
+}
